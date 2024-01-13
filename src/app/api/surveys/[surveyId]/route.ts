d97204d59e59c8636dd36d4652c27d84585d58d4
davidpetkovski <petkovski.a22007@next.edu.mk>
2024-01-13 16:32:45 +0100

    Implement find one survey endpoint

---
+++ src/app/api/surveys/[surveyId]/route.ts
@@ -0,0 +1,41 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+type ApiHandlerContext = {
+  params: {
+    surveyId: string;
+  };
+};
+
+export async function GET(request: NextRequest, context: ApiHandlerContext) {
+  const { surveyId } = context.params;
+  try {
+    const survey = await prisma.survey.findUniqueOrThrow({
+      where: {
+        id: surveyId,
+      },
+    });
+    return NextResponse.json({
+      data: survey,
+    });
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      return NextResponse.json(
+        {
+          error: e.message,
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+    return NextResponse.json(
+      {
+        error: "Unknown error occured",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
